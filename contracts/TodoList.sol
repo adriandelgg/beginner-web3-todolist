702d8c6261eb595a0a1e30c8cbdd7fdae42aeb36
adriandelgg <adriandelgadoc1@gmail.com>
2021-05-18 23:53:30 -0700

    Added Web3.js & Truffle Tests

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -2,4 +2,31 @@ pragma solidity ^0.5.0;
 
 contract TodoList {
     uint256 public taskCount = 0;
+
+    struct Task {
+        uint256 id;
+        string content;
+        bool completed;
+    }
+
+    mapping(uint256 => Task) public tasks;
+    event TaskCreated(uint256 id, string content, bool completed);
+
+    constructor() public {
+        createTask("Study Solidity");
+    }
+
+    function createTask(string memory _content) public {
+        taskCount++;
+        tasks[taskCount] = Task(taskCount, _content, false);
+        emit TaskCreated(taskCount, _content, false);
+    }
+
+    function completeTask(uint256 _taskId, bool _completed) public {
+        tasks[_taskId].completed = _completed;
+    }
+
+    // function kill() public onlyOwner {
+    //     selfdestruct(owner());
+    // }
 }
--- truffle-config.js
+++ truffle-config.js
@@ -5,11 +5,20 @@ module.exports = {
 			port: 7545,
 			network_id: '*' // Match any network id
 		}
+		// rinkeby: {
+		// 	provider: function() {
+		// 		return new HDWalletProvider(, "https://rinkeby.infura.io/v3/35903f94ae1640f39995a7e845c396c8");
+		// 	},
+		// 	network_id: 4
+		// }
 	},
-	solc: {
-		optimizer: {
-			enabled: true,
-			runs: 200
+	compilers: {
+		solc: {
+			version: '^0.5.0',
+			optimizer: {
+				enabled: true,
+				runs: 200
+			}
 		}
 	}
 };
